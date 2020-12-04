47cb4f9d0f3305a8195da3248cd943b1eb9456ab
petern1953 <petern1953@gmail.com>
2020-12-04 08:38:29 +0100

    temp files to study animation

--- pair-game.js
+++ pair-game.js
@@ -1,10 +1,10 @@
 // Egy “Keresd a párját” játékot kell készítened.
-// A játékban két sorban soronként 5 azaz összesen 10 darab kártya látható.
+// A játékban két sorban soronként 5 -- azaz összesen 10 darab -- kártya látható.
 // A játék kezdetekor a kártyák le vannak fordítva. A kártyák másik oldala egy egyszerű ábrát tartalmaz.
 // Az ábrák szabadon választható képek vagy akár ikonok is lehetnek a könnyebb felismerhetőség végett.
 // Minden ábrából összesen két darab van.
 // Amikor a játékos rákattint egy kártyára, az egy egyszerű animáció kíséretében megfordul, felfedve az ábrát.
-// Az első kártyára való kattintáskor elindul egy számláló, ami a játékidőt mutatja perc / másodperc formában.
+// Az első kártyára való kattintáskor elindul egy számláló, amely a játékidőt mutatja perc:másodperc ( mm:ss ) formában.
 // Amennyiben egymás után két felfordított kártyán ugyanaz az ábra szerepel, azokat felfordítva kell hagyni a játék végéig, 
 // többet nem lehet rájuk kattintani.
 // Amennyiben a két ábra eltérő, mind a kettőt automatikusan vissza kell forgatni.
